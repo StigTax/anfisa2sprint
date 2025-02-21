a1ba5c6236bf6fdf1990f1ba9ed188beec168748
Никита Ефремчев <efremchef@yandex.ru>
2025-02-21 22:28:48 +0300

    second iteration educational project

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -53,6 +53,15 @@ class IceCream(PublishedModel):
     )
     title = models.CharField(max_length=256, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+    price = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -72,6 +81,7 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = 'Мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
